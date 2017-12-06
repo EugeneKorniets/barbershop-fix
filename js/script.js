b8dab32d77b7acea194abce8ba27259718c1bbd3
EugeneKorniets <evgeniy.korniets@gmail.com>
2017-12-06 21:43:23 +0300

    Bug fixes JS

--- js/script.js
+++ js/script.js
@@ -1,74 +1,111 @@
 // работа с модальными окнами start
 
-// объявление переменных для работы с модальными окнами
-var loginBtn = document.querySelector(".login");
-var feedbackBtn = document.querySelector(".feedback-btn");
-var loginPopup = document.querySelector(".modal-content");
-var feedbackModal = document.querySelector(".modal-feedback");
-var modalOverlay = document.querySelector(".modal-overlay");
-var login = loginPopup.querySelector("[name=login]");
+// модальное окно login
+if (document.querySelector(".modal-content") != null) {
+  var loginBtn = document.querySelector(".login");
+  var loginPopup = document.querySelector(".modal-content");
+  var modalOverlay = document.querySelector(".modal-overlay");
+  var login = loginPopup.querySelector("[name=login]");
+  var form = loginPopup.querySelector("form");
+  var password = loginPopup.querySelector("[name=password]");
+  var closeLogin = document.querySelector(".modal-content .modal-content-close");
 
-var form = loginPopup.querySelector("form");
-var password = loginPopup.querySelector("[name=password]");
-
-var closeLogin = document.querySelector(".modal-content .modal-content-close");
-var closeFeedback = document.querySelector(".modal-feedback-close");
-
-var mapBtn = document.querySelector(".js-btn-map");
-var mapPopup = document.querySelector(".modal-content-map");
-
-var mapBtnFooter = document.querySelector(".footer .js-btn-map");
-
-var closeMap = document.querySelector(".modal-content-map .modal-content-close");
-
-// появление окна авторизации при клике по кнопке Вход, фокусировка на поле Логин
-loginBtn.addEventListener("click", function(event){
-  event.preventDefault();
-  modalOverlay.classList.add("modal-overlay-show");
-  loginPopup.classList.add("modal-content-show");
-  login.focus();
-});
-
-// появление окна обратной связи при клике на кнопке Обратная связь на главной
-feedbackBtn.addEventListener("click", function(event){
-  event.preventDefault();
-  modalOverlay.classList.add("modal-overlay-show");
-  feedbackModal.classList.add("modal-feedback-show");
-});
-
-// проверка формы на заполненность полей при попытке отправки формы
-form.addEventListener("submit", function(event) {
-  if (!login.value || !password.value) {
+  // появление окна login при клике по кнопке Вход, фокусировка на поле Логин
+  loginBtn.addEventListener("click", function(event){
     event.preventDefault();
-        loginPopup.classList.add("modal-error"); // не забываем удалить при закрытии окна
-      }
-    });
+    modalOverlay.classList.add("modal-overlay-show");
+    loginPopup.classList.add("modal-content-show");
+    login.focus();
+  });
 
-// скрытие окна авторизации при клике по кнопке Закрыть модального окна авторизации
-closeLogin.addEventListener("click", function(event) {
-  event.preventDefault();
-  modalOverlay.classList.remove("modal-overlay-show");
-  loginPopup.classList.remove("modal-content-show");
-  loginPopup.classList.remove("modal-error");
-});
+  // проверка формы на заполненность полей при попытке отправки формы
+  form.addEventListener("submit", function(event) {
+    if (!login.value || !password.value) {
+      event.preventDefault();
+      loginPopup.classList.add("modal-error"); // не забываем удалить при закрытии окна
+    }
+  });
 
-// скрытие окна обратной связи при клике по кнопке Закрыть модального окна обратной связи
-closeFeedback.addEventListener("click", function(event) {
-  event.preventDefault();
-  modalOverlay.classList.remove("modal-overlay-show");
-  feedbackModal.classList.remove("modal-feedback-show");
-});
+  // скрытие окна login при клике по кнопке Закрыть модального окна
+  closeLogin.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalOverlay.classList.remove("modal-overlay-show");
+    loginPopup.classList.remove("modal-content-show");
+    loginPopup.classList.remove("modal-error");
+  });
+}
 
-// скрытие всех окон при клике вне модального окна
+
+// модальное окно feedback
+if (document.querySelector(".modal-feedback") != null) {
+  var feedbackBtn = document.querySelector(".feedback-btn");
+  var feedbackModal = document.querySelector(".modal-feedback");
+  var closeFeedback = document.querySelector(".modal-feedback-close");
+
+  // появление окна feedback при клике на кнопке Обратная связь на главной
+  feedbackBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    modalOverlay.classList.add("modal-overlay-show");
+    feedbackModal.classList.add("modal-feedback-show");
+  });
+
+  // скрытие окна feedback при клике по кнопке Закрыть модального окна
+  closeFeedback.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalOverlay.classList.remove("modal-overlay-show");
+    feedbackModal.classList.remove("modal-feedback-show");
+  });
+}
+
+
+// модальное окно map
+if (document.querySelector(".modal-content-map") != null) {
+  var mapBtn = document.querySelector(".js-btn-map");
+  var mapBtnFooter = document.querySelector(".footer .js-btn-map");
+  var mapPopup = document.querySelector(".modal-content-map");
+  var closeMap = document.querySelector(".modal-content-map .modal-content-close");
+
+  // появление окна map при клике по кнопке Как проехать
+  mapBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalOverlay.classList.add("modal-overlay-show");
+    mapPopup.classList.add("modal-content-map-show");
+  });
+
+  // появление окна map при клике по кнопке Как нас найти в футере
+  mapBtnFooter.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalOverlay.classList.add("modal-overlay-show");
+    mapPopup.classList.add("modal-content-map-show");
+  });
+
+  // скрытие окна map, при клике по кнопке Закрыть модального окна
+  closeMap.addEventListener("click", function(event) {
+    event.preventDefault();
+    modalOverlay.classList.remove("modal-overlay-show");
+    mapPopup.classList.remove("modal-content-map-show");
+  });
+}
+
+
+// скрытие всех модальных окон при клике вне модального окна
 modalOverlay.addEventListener("click", function(event) {
-  event.preventDefault();
-  modalOverlay.classList.remove("modal-overlay-show");
-  loginPopup.classList.remove("modal-content-show");
-  loginPopup.classList.remove("modal-error");
+  if (document.querySelector(".modal-content") != null) {
+    modalOverlay.classList.remove("modal-overlay-show");
+    loginPopup.classList.remove("modal-content-show");
+    loginPopup.classList.remove("modal-error");
+  }
 
-  feedbackModal.classList.remove("modal-feedback-show");
+  if(document.querySelector(".modal-feedback") != null) {
+    if (feedbackModal.classList.contains("modal-feedback-show")) {
+      modalOverlay.classList.remove("modal-overlay-show");
+      feedbackModal.classList.remove("modal-feedback-show");
+    }
+  }
 
-  mapPopup.classList.remove("modal-content-map-show");
+  if(document.querySelector(".modal-content-map") != null) {
+    mapPopup.classList.remove("modal-content-map-show");
+  }
 });
 
 // скрытие всех модальных окон при нажатии клавиши ESC
@@ -83,46 +120,25 @@ window.addEventListener("keydown", function(event) {
       modalOverlay.classList.remove("modal-overlay-show");
       mapPopup.classList.remove("modal-content-map-show");
     }
-    if (feedbackModal.classList.contains("modal-feedback-show")) {
-      modalOverlay.classList.remove("modal-overlay-show");
-      feedbackModal.classList.remove("modal-feedback-show");
+    if(document.querySelector(".modal-feedback") != null) {
+      if (feedbackModal.classList.contains("modal-feedback-show")) {
+        modalOverlay.classList.remove("modal-overlay-show");
+        feedbackModal.classList.remove("modal-feedback-show");
+      }
     }
   }
-});
-
-// появление окна с картой при клике по кнопке Как проехать
-mapBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  modalOverlay.classList.add("modal-overlay-show");
-  mapPopup.classList.add("modal-content-map-show");
-});
-
-
-// появление окна с картой при клике по кнопке Как нас найти в футере
-mapBtnFooter.addEventListener("click", function(event) {
-  event.preventDefault();
-  modalOverlay.classList.add("modal-overlay-show");
-  mapPopup.classList.add("modal-content-map-show");
-});
-
-
-// скрытие окна с картой, при клике по кнопке Закрыть модального окна
-closeMap.addEventListener("click", function(event) {
-  event.preventDefault();
-  modalOverlay.classList.remove("modal-overlay-show");
-  mapPopup.classList.remove("modal-content-map-show");
 });
 
 // работа с модальными окнами end
 
 // работа слайдера start
-var galleryImage = document.querySelectorAll(".gallery-image");
-var galleryBtnPrev = document.querySelector(".gallery-prev");
-var galleryBtnNext = document.querySelector(".gallery-next");
-var countGalleryImages = galleryImage.length;
-var activeGalleryImage;
+if (document.querySelectorAll(".gallery-image").length > 0) {
+  var galleryImage = document.querySelectorAll(".gallery-image");
+  var galleryBtnPrev = document.querySelector(".gallery-prev");
+  var galleryBtnNext = document.querySelector(".gallery-next");
+  var countGalleryImages = galleryImage.length;
+  var activeGalleryImage;
 
-if (countGalleryImages > 0) {
   toggleButtons();
 
   galleryBtnNext.addEventListener('click', function(){
@@ -163,19 +179,16 @@ if (countGalleryImages > 0) {
       galleryBtnPrev.disabled = false;
     }
   });
-} else {
-  galleryBtnPrev.disabled = true;
-  galleryBtnNext.disabled = true;
-};
 
-function toggleButtons () {
-  if (galleryImage[0].classList.contains("active")) {
-    galleryBtnPrev.disabled = true;
-  }
+  function toggleButtons () {
+    if (galleryImage[0].classList.contains("active")) {
+      galleryBtnPrev.disabled = true;
+    }
 
-  if (galleryImage[countGalleryImages - 1].classList.contains("active")) {
-    galleryBtnNext.disabled = true;
-  }
-};
+    if (galleryImage[countGalleryImages - 1].classList.contains("active")) {
+      galleryBtnNext.disabled = true;
+    }
+  };
+}
 
 // работа слайдера end
